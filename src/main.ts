a8f3ea44daf911c9bfa0763fde3b46de41d908d6
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-05-06 01:21:34 +0800

    refactor: extract router code

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import { createStore } from 'vuex'
 
 import App from './App.vue'
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import ColumnDetail from './views/ColumnDetail.vue'
-
-const routerHistory = createWebHistory()
-const router = createRouter({
-  history: routerHistory,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/column/:id',
-      name: 'column',
-      component: ColumnDetail
-    }
-  ]
-})
+import router from './router'
 
 const store = createStore({
   state: {
