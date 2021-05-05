c42d976f837c449a3bc6849c372daaced70f385b
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-05-06 01:07:46 +0800

    feat: add vuex

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createStore } from 'vuex'
 
 import App from './App.vue'
 import Home from './views/Home.vue'
@@ -28,6 +29,19 @@ const router = createRouter({
   ]
 })
 
+const store = createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add (state) {
+      state.count++
+    }
+  }
+})
+console.log('store', store.state.count)
+store.commit('add')
+console.log('store', store.state.count)
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
