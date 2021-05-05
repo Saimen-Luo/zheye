9f5da828aaff728a1cb4c6015c0e7ef4cdad4eeb
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-05-05 22:38:55 +0800

    feat: add PostList and finish ColumnDetail component

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,17 +1,45 @@
 <template>
-  <pre>{{ route }}</pre>
+  <div class="column-detail-page w-75 mx-auto">
+    <div
+      class="column-info row mb-4 border-bottom pb-4 align-items-center"
+      v-if="column"
+    >
+      <div class="col-3 text-center">
+        <img
+          :src="column.avatar && column.avatar"
+          :alt="column.title"
+          class="rounded-circle border w-100"
+        />
+      </div>
+      <div class="col-9">
+        <h4>{{ column.title }}</h4>
+        <p class="text-muted">{{ column.description }}</p>
+      </div>
+    </div>
+    <post-list :list="posts" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useRoute } from 'vue-router'
 
+import { testColumns, testPosts } from '../testData'
+import PostList from '../components/PostList.vue'
+
 export default defineComponent({
   name: 'ColumnDetail',
+  components: {
+    PostList
+  },
   setup () {
     const route = useRoute()
+    const currentId = +route.params.id
+    const column = testColumns.find(c => c.id === currentId)
+    const posts = testPosts.filter(p => p.columnId === currentId)
     return {
-      route
+      column,
+      posts
     }
   }
 })
