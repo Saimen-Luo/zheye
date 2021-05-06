089c072bda2945d2a2b69e922e9ed111ae184bd3
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2021-05-06 17:43:01 +0800

    feat: add router Navigation Guards

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import ColumnDetail from './views/ColumnDetail.vue'
 import CreatePost from './views/CreatePost.vue'
+import store from './store'
 
 const routerHistory = createWebHistory()
 const router = createRouter({
@@ -30,6 +31,14 @@ const router = createRouter({
       component: CreatePost
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'login' && !store.state.user.isLogin) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
